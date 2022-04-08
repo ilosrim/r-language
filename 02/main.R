33c20016b6d5df620f1c18bb9e308696e0274eb8
ilosrim <ilosrim@yahoo.com>
2022-04-09 01:22:05 +0500

    data types

---
+++ 02/main.R
@@ -0,0 +1,12 @@
+# Data Types
+
+# decimals, integers, text.
+
+# decimal (numeric) - type double
+var1 <- 3.14
+
+# integer - type double
+var2 <- 20
+
+# text - type character
+var3 <- "Hello World!"
