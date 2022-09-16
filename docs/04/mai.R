666b109a8a98f247d406136e25da765d17965ced
ilosrim <ilosrim@yahoo.com>
2022-09-16 10:34:45 -0700

    edited

---
+++ docs/04/mai.R
@@ -0,0 +1,40 @@
+# Decision Making
+x <- 24
+if(x>100){
+  print("x is big")
+} else {
+  print("x less than 100")
+}
+
+# Logical operators
+a <- 5
+b <- 4
+if(a>b & a>10){ # OR = | # nolint
+  print("Yes")
+}else{
+  print("No")
+}
+
+y <- TRUE
+#outputs FALSE
+print(!y)
+
+num <- 3
+result <- switch(
+  num,
+  "One",
+  "Two",
+  "Three",
+  "Four"
+)
+print(result)
+
+c <- "c"
+res <- switch(
+  c,
+  "a" = "One",
+  "b" = "Two",
+  "c" = "Three",
+  "d" = "Four"
+)
+print(res)
